bdad466e1a3f2efc2e417e43bc9e3ba575afb94c
nikkudadhich <110150206+nikkudadhich@users.noreply.github.com>
2026-01-09 12:08:57 +0530

    Music file
    You can add as much some you wanted.

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "meri" : "https://www.youtube.com/watch?v=RPFkaHG2ONA&list=RDRPFkaHG2ONA&start_radio=1",
+    "bipasha" : "https://www.youtube.com/watch?v=DhytQnmTtxw&list=RDDhytQnmTtxw&start_radio=1&pp=ygUMYmlwYXNoYSBzb25noAcB",
+    "mera" : "https://www.youtube.com/watch?v=xVViRaALOLM&list=RDxVViRaALOLM&start_radio=1",
+    "mala" : "https://www.youtube.com/watch?v=eYSaHXXFIBU&list=RDeYSaHXXFIBU&start_radio=1"
+}
